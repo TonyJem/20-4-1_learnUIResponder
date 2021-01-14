58498cf45ccde384626ea45eedc18297b6b03168
TonyJem <a.jemanov@gmail.com>
2021-01-14 22:32:06 +0200

    Remove unnecessary comments and spaces

--- petUIResponder/petUIResponder/SceneDelegate.swift
+++ petUIResponder/petUIResponder/SceneDelegate.swift
@@ -1,10 +1,3 @@
-//
-//  SceneDelegate.swift
-//  petUIResponder
-//
-//  Created by Office-iMac on 2021-01-14.
-//
-
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
@@ -46,7 +39,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
 }
 
